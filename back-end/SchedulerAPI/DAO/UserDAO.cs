674af390282a826a501dc807ab1fd8b2dd6e5c46
Nguyen Trong <nguyentrong.se11@gmail.com>
2025-05-10 19:26:56 +0700

    update code base backend

---
+++ back-end/SchedulerAPI/DAO/UserDAO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerAPI.DataContext;
+using SchedulerAPI.Model;
+
+namespace SchedulerAPI.DAO
+{
+    public class UserDAO
+    {
+        public async Task<List<User>> GetAllUsers()
+        {
+            using (var context = new SchedulerContext())
+            {
+                return await context.Users.ToListAsync();
+            }
+        }
+    }   
+}
---
+++ back-end/SchedulerAPI/DataContext/SchedulerContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulerAPI.Model;
+
+namespace SchedulerAPI.DataContext
+{
+    public class SchedulerContext : DbContext
+    {
+        public SchedulerContext()
+        {
+        }
+        public SchedulerContext(DbContextOptions<SchedulerContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                IConfiguration configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User 
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "example@gmail.com",
+            });
+        }
+    }
+}
